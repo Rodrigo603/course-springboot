6647134b9407a9b0b45505a1fb51857525a62f76
Rodrigo Dantas <rodrigo6031@gmail.com>
2025-09-18 14:23:03 -0300

    Order item, many-to-many association with extra attributes

--- src/main/java/com/rjdo/course/entities/Order.java
+++ src/main/java/com/rjdo/course/entities/Order.java
@@ -2,17 +2,21 @@ package com.rjdo.course.entities;
 
 import java.io.Serializable;
 import java.time.Instant;
+import java.util.HashSet;
 import java.util.Objects;
+import java.util.Set;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.rjdo.course.entities.enums.OrderStatus;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Entity
@@ -34,6 +38,10 @@ public class Order implements Serializable {
 	@JoinColumn(name = "client_id")
 	private User client;
 
+	@OneToMany(mappedBy = "id.order", fetch = FetchType.EAGER)
+	private Set<OrderItem> items = new HashSet<>();
+		
+	
 	public Order() {
 	}
 
@@ -79,6 +87,10 @@ public class Order implements Serializable {
 		this.client = client;
 	}
 
+	public Set<OrderItem> getItems() {
+		return items;
+	}
+	
 	@Override
 	public int hashCode() {
 		return Objects.hash(id);
