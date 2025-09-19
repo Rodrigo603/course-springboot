1c4b0f96d7e16b5659f455d54d9a4d0e2449662a
Rodrigo Dantas <rodrigo6031@gmail.com>
2025-09-19 20:46:29 -0300

    Error handling - findById

---
+++ src/main/java/com/rjdo/course/services/exceptions/ResourceNotFoundException.java
@@ -0,0 +1,10 @@
+package com.rjdo.course.services.exceptions;
+
+public class ResourceNotFoundException extends RuntimeException {
+
+	private static final long serialVersionUID = 1L;
+	
+	public ResourceNotFoundException(Object id) {
+		super("Resource not found. Id: " + id);
+	}
+}
